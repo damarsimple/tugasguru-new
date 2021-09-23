34738b57a1c049f006fb258962f5096b6fb79380
Damar Albaribin <damaralbaribin@gmail.com>
2021-09-23 12:56:00 +0700

    build fix

--- components/PaperLoading.tsx
+++ components/PaperLoading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiOutlineLoading } from "react-icons/ai";
 import Paper from "./Paper";
 
-export default function PaperLoading(e) {
+export default function PaperLoading() {
   return (
     <Paper name="Loading..." className="flex justify-center">
       <AiOutlineLoading className="animate-spin" />
